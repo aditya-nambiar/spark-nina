9ae8fa6c84c8a9f1fc3758ebda079c77ce5ad48a
aditya-nambiar <aditya.nambiar007@gmail.com>
2014-05-15 22:34:04 +0530

    Changes

--- src/edu/nd/nina/test/ApproxDiameter.scala
+++ src/edu/nd/nina/test/ApproxDiameter.scala
@@ -2,6 +2,48 @@ package edu.nd.nina.test
 
 import org.apache.spark.SparkContext
 import org.apache.spark.graphx._
+import java.util.Random
+import scala.collection.mutable.ArrayBuffer
+
+object gbl{
+def myrand() : Float  = {
+  var r = new scala.util.Random
+  return  r.nextInt(10000) / 10001
+}
+def hash_value(): Int = {
+  var  ret:Int = 0;
+  while (myrand() < 0.5) {
+    ret= ret +1
+  }
+  return ret;
+}
+}
+class vdata() {
+	  var bitmask1 = new ArrayBuffer[ArrayBuffer[Boolean]]
+      val bitmask2 = new ArrayBuffer[ArrayBuffer[Boolean]]
+       /** for exact counting (but needs large memory) */
+      def create_bitmask(id: Int){
+          val mask1 = new ArrayBuffer[Boolean](id+2)
+	      mask1(id) = true
+	      bitmask1 += mask1
+	      val mask2 = new ArrayBuffer[Boolean](id+2)
+	      mask2(id) = true
+	      bitmask2 += mask2
+      }
+      def create_hashed_bitmask( id: Int) {
+    for (i <- 1 until 10) { /** DUPULICATION_OF_BITMASKS = 10 */
+      var  hash_val :Int  = gbl.hash_value();
+      val mask1= new ArrayBuffer[Boolean](hash_val+2)
+      mask1(hash_val) = true
+      bitmask1 += mask1
+      val mask2= new ArrayBuffer[Boolean](hash_val+2)
+      mask2(id)=true
+      bitmask2 += mask2
+      
+
+    }
+  }
+}
 
 
 object ApproxDiameter {
@@ -18,7 +60,7 @@ object ApproxDiameter {
 
     val pagerankGraph: Graph[vdata, Int] = graph
       // Associate the degree with each vertex
-      .mapVertices( (id, attr) => vdata() )
+      .mapVertices( (id, attr) => vdata )
     
 
   }
@@ -29,15 +71,6 @@ object ApproxDiameter {
       (newPR, newPR - oldPR)
     }
   
-}
-
-class vdata () {
-      val bitmask1: Vector[Vector[Boolean]] = Vector();
-      val bitmask2: Vector[Vector[Boolean]] = Vector();
- 
-      def createbitmask(id: VertexId){
-        
-      }
 }
 
 

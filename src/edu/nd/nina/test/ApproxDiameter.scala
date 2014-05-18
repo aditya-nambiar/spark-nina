2247543c9812dbd19db80b04e681b98b818ac8a0
aditya-nambiar <aditya.nambiar007@gmail.com>
2014-05-18 05:43:26 -0500

    Confilcit resolved

--- src/edu/nd/nina/test/ApproxDiameter.scala
+++ src/edu/nd/nina/test/ApproxDiameter.scala
@@ -2,56 +2,12 @@ package edu.nd.nina.test
 
 import org.apache.spark.SparkContext
 import org.apache.spark.graphx._
-<<<<<<< HEAD
-import java.util.Random
-import scala.collection.mutable.ArrayBuffer
 
-object gbl{
-def myrand() : Float  = {
-  var r = new scala.util.Random
-  return  r.nextInt(10000) / 10001
-}
-def hash_value(): Int = {
-  var  ret:Int = 0;
-  while (myrand() < 0.5) {
-    ret= ret +1
-  }
-  return ret;
-}
-}
-class vdata() {
-	  var bitmask1 = new ArrayBuffer[ArrayBuffer[Boolean]]
-      val bitmask2 = new ArrayBuffer[ArrayBuffer[Boolean]]
-       /** for exact counting (but needs large memory) */
-      def create_bitmask(id: Int){
-          val mask1 = new ArrayBuffer[Boolean](id+2)
-	      mask1(id) = true
-	      bitmask1 += mask1
-	      val mask2 = new ArrayBuffer[Boolean](id+2)
-	      mask2(id) = true
-	      bitmask2 += mask2
-      }
-      def create_hashed_bitmask( id: Int) {
-    for (i <- 1 until 10) { /** DUPULICATION_OF_BITMASKS = 10 */
-      var  hash_val :Int  = gbl.hash_value();
-      val mask1= new ArrayBuffer[Boolean](hash_val+2)
-      mask1(hash_val) = true
-      bitmask1 += mask1
-      val mask2= new ArrayBuffer[Boolean](hash_val+2)
-      mask2(id)=true
-      bitmask2 += mask2
-      
-
-    }
-  }
-}
-
-=======
 import scala.collection.mutable.ArrayBuffer
 import scala.util.control.Breaks._
 import scala.collection.mutable.ListBuffer
 import scala.util.Random
->>>>>>> upstream/master
+
 
 object ApproxDiameter {
   def main(args: Array[String]) {
